34ea279f3b34235da4decb676793fc3e62e1777f
Ajisaputrars <ajisaputrars@gmail.com>
2017-12-27 16:44:54 +0700

    Initial Commit - Storyboard Ready

---
+++ MovieApp/Controller/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Aji Saputra Raka Siwi on 12/27/17.
+//  Copyright © 2017 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collection.delegate = self
+        collection.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "weatherCell", for: indexPath) as? WeatherCell {
+            
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "detailSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailSegue" {
+            if let detailsVC = segue.destination as? DetailVC {
+                
+            }
+        }
+    }
+
+    
+}
---
+++ MovieApp/View/WeatherCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherCell.swift
+//  MovieApp
+//
+//  Created by Aji Saputra Raka Siwi on 12/27/17.
+//  Copyright © 2017 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UICollectionViewCell {
+
+}
